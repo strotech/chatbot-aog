3ea1fa89dc59cf81263e9311fef6e3a26a0d22aa
strotech <sijils@outlook.com>
2020-04-20 21:46:18 +0530

    integrating sentry

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ var app=express();
 var bodyParser = require('body-parser');
 
 Sentry.init({ dsn: 'https://7a71c49c396a4093af6c086702617fa2@o380166.ingest.sentry.io/5205698' });
+
 app.use(Sentry.Handlers.requestHandler());
+
 
 //configure body-parser for express
 app.use(bodyParser.urlencoded({extended:false}));
@@ -15,6 +17,7 @@ var routes=require('./routes.js');
 app.use('/', routes);
 
 app.use(Sentry.Handlers.errorHandler());
+
 // eslint-disable-next-line no-unused-vars
 app.use(function onError(err, req, res, next) {
     // The error id is attached to `res.sentry` to be returned
