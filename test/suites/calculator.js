add3358eab84d2e4c3413e6c9eb670d89413b2b8
strotech <sijils@outlook.com>
2020-04-25 19:48:55 +0530

    minor fixes and series file

--- test/suites/calculator.js
+++ test/suites/calculator.js
@@ -1,6 +1,6 @@
 const app=require('src/app')
 const supertest = require('supertest')
-const request = supertest(app)
+const testapp = supertest(app)
 
 const addRequest=require('test/resources/calculator/request/addition.json')
 const subtractionRequest=require('test/resources/calculator/request/subtraction.json')
@@ -11,20 +11,20 @@ const subtractionResponse=require('test/resources/calculator/response/subtractio
 
 describe("Calculator chat API", () => {
     test('Gets the server endpoint', async done => {
-        const res = await request.get('/')
+        const res = await testapp.get('/')
         expect(res.status).toBe(200)
         expect(res.text).toBe('Server is up and running')
         done()
     })
     test('Post addition request',async done =>{
-        const res = await request.post('/calculator')
+        const res = await testapp.post('/calculator')
             .send(addRequest)
         expect(res.status).toBe(200)
         expect(res.body).toEqual(addResponse)
         done()
     })
     test('Post subtraction request',async done =>{
-        const res = await request.post('/calculator')
+        const res = await testapp.post('/calculator')
             .send(subtractionRequest)
         expect(res.status).toBe(200)
         expect(res.body).toEqual(subtractionResponse)
